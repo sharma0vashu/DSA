b31e937720b8177290d4e9ee6ee01837ca0a7283
sharma0vashu <149711363+sharma0vashu@users.noreply.github.com>
2025-01-05 00:34:05 +0530

    Time: 0 ms (100.00%) | Memory: 40 MB (73.53%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,37 +1,19 @@
 class Solution {
 public:
-   vector<int> productExceptSelf(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> ans(n, 0);
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n,1);
+        int prefix=1;
+        for(int i=0;i<n;i++){
+        ans[i] = prefix;
+            prefix *= nums[i];
+        }
+        int suffix=1;
+        for(int i=n-1;i>=0;i--){
+            ans[i] *= suffix;
+            suffix *= nums[i];
+        }
         
-        int totalProduct = 1;  // Product of all non-zero elements
-        int zeroCount = 0;     // Count of zeros in the array
-
-        // Calculate total product and count zeros
-        for (int val : nums) {
-            if (val == 0) {
-                zeroCount++;
-            } else {
-                totalProduct *= val;
-            }
-        }
-
-        // Handle cases based on the number of zeros
-        for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                // If the current number is zero and there's only one zero
-                if (zeroCount == 1) {
-                    ans[i] = totalProduct;
-                }
-                // If there are more than one zero, product is zero
-            } else {
-                // If no zeros in the array
-                if (zeroCount == 0) {
-                    ans[i] = totalProduct / nums[i];
-                }
-                // If there is one zero, product at non-zero indices is zero
-            }
-        }
         return ans;
     }
 };
