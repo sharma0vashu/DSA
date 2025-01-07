2689e67401f4dcc2183ca960726cee1192a07180
sharma0vashu <149711363+sharma0vashu@users.noreply.github.com>
2025-01-07 22:59:44 +0530

    Time: 0 ms (100.00%) | Memory: 63.5 MB (56.25%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.cpp
@@ -6,9 +6,8 @@ public:
         while (st < end) {
             int mid = st + (end - st) / 2;
 
-            // Compare mid with its next element
             if (arr[mid] > arr[mid + 1]) {
-                // Peak is on the left side, including mid
+               
                 end = mid;
             } else {
                 // Peak is on the right side
