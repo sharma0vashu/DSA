03b61aa17bfc703590a9c418ad2a02383369e79e
sharma0vashu <149711363+sharma0vashu@users.noreply.github.com>
2025-01-10 21:53:20 +0530

    Time: 0 ms (100.00%) | Memory: 12 MB (23.65%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -3,25 +3,13 @@ public:
     vector<int> sortColors(vector<int>& arr) {
         int n = arr.size();
 
-    for (int i = 0; i < n - 1; ++i) {
-
-        // Assume the current position holds
-        // the minimum element
+    for (int i = 0; i < n - 1; i++) {
         int min_idx = i;
-
-        // Iterate through the unsorted portion
-        // to find the actual minimum
-        for (int j = i + 1; j < n; ++j) {
+        for (int j = i + 1; j < n; j++) {
             if (arr[j] < arr[min_idx]) {
-
-                // Update min_idx if a smaller
-                // element is found
                 min_idx = j; 
             }
         }
-
-        // Move minimum element to its
-        // correct position
         swap(arr[i], arr[min_idx]);
     }
     return arr;
