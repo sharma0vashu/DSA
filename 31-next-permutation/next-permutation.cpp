519839493656d405dee62b17f3850b7d82b1d4bb
sharma0vashu <149711363+sharma0vashu@users.noreply.github.com>
2025-01-15 23:48:36 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (26.57%) - LeetSync

--- 31-next-permutation/next-permutation.cpp
+++ 31-next-permutation/next-permutation.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
-    void nextPermutation(vector<int>& A) {
-        next_permutation(A.begin(),A.end());
+    vector<int> nextPermutation(vector<int>& A) {
+        int n = A.size();
+        int p = -1;
+
+        // Step 1: Find the first decreasing element from the end
+        for (int i = n - 2; i >= 0; i--) { // Changed i=n-1 to i=n-2
+            if (A[i] < A[i + 1]) {
+                p = i;
+                break;
+            }
+        }
+
+        // Step 2: If no such element is found, reverse the array
+        if (p == -1) {
+            reverse(A.begin(), A.end());
+            return A;
+        }
+
+        // Step 3: Find the smallest element greater than A[p] to the right of p
+        for (int i = n - 1; i > p; i--) {
+            if (A[i] > A[p]) {
+                swap(A[i], A[p]);
+                break;
+            }
+        }
+
+        // Step 4: Reverse the elements to the right of p
+        reverse(A.begin() + p + 1, A.end());
+        return A;
     }
 };
