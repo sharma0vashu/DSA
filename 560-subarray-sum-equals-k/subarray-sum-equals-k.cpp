c06844cf795744ddcd91336c4f6a692e299a3706
sharma0vashu <149711363+sharma0vashu@users.noreply.github.com>
2025-01-16 18:20:58 +0530

    Time: 50 ms (56.36%) | Memory: 45.3 MB (74.71%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,5 +1,4 @@
-#include <vector>
-#include <unordered_map>
+
 using namespace std;
 
 class Solution {
@@ -7,7 +6,7 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> prefixSum;
         int sum = 0, count = 0;
-        prefixSum[0] = 1; // Initialize with a prefix sum of 0
+        prefixSum[0] = 1; 
         
         for (int num : nums) {
             sum += num;
