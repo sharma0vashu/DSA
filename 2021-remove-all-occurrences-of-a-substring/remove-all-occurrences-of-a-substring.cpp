2d31911da52b0801372850689cca913960d518ca
sharma0vashu <149711363+sharma0vashu@users.noreply.github.com>
2025-02-12 00:51:37 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (34.81%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     string removeOccurrences(string s, string part) {
       size_t pos;
-    while ((pos = s.find(part)) != std::string::npos) { // Find leftmost occurrence
+    while ((pos = s.find(part)) != string::npos) { // Find leftmost occurrence
         s.erase(pos, part.length()); // Remove the found occurrence
     }
     return s;
