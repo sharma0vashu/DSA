72dadc29440cee5f60907481183a38eb3c774a14
Vashu Sharma <149711363+sharma0vashu@users.noreply.github.com>
2025-02-14 17:04:39 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (69.33%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.cpp
+++ 1666-make-the-string-great/make-the-string-great.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
     string makeGood(string s) {
+        int n=s.length();
         string result = "";
+         if(n==0 || n==1){
+           return s; 
+        }
         for (char &ch : s) {
             if (!result.empty() && (result.back() - 32 == ch || result.back() + 32 == ch)) {
                 result.pop_back();
