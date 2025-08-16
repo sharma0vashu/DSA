46066ca67b7874bb9fb0573423d99161c3168750
Vashu Sharma <149711363+sharma0vashu@users.noreply.github.com>
2025-08-16 18:38:10 +0530

    Time: 0 ms (100.00%) | Memory: 8.7 MB (13.50%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    double result(double x, long long n) { 
-        double res = 1.0;
-        while (n > 0) {
-            if (n % 2 == 1) res *= x; // Multiply when n is odd
-            x *= x; // Square x
-            n /= 2; // Reduce exponent by half
+    double myPow(double x, int n) {
+        if(n==0) return 1;
+        if(n==1) return x;
+        
+         long long exp = n;  
+        if (exp < 0) {
+            x = 1 / x;
+            exp = -exp;
         }
-        return res;
-    }
-
-    double myPow(double x, long long n) {
-        if (n == 0) return 1; 
-        if (x == 1) return 1;
-        if (x == -1) return (n % 2 == 0) ? 1 : -1; // Even power gives 1, odd gives -1
-
-        if (n < 0) {
-            return 1 / result(x, -n); // Handle negative exponent properly
-        } else {
-            return result(x, n);
+        double ans = myPow(x,exp/2);
+        if(n%2==0){
+            return ans*ans;
+        }
+        else{
+            return x*ans*ans;
         }
     }
 };
