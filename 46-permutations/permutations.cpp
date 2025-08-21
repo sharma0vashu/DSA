55e3162e621538e3e96e357eb96ac577153cf8a5
Vashu Sharma <149711363+sharma0vashu@users.noreply.github.com>
2025-08-21 21:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (46.86%) - LeetSync

---
+++ 46-permutations/permutations.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void prmu(vector<int> &nums, vector<vector<int>> &ans, int pos) {
+        if (pos >= nums.size()) {
+            ans.push_back(nums);
+            return;
+        }
+        for (int i = pos; i < nums.size(); i++) {
+            swap(nums[i], nums[pos]);
+            prmu(nums, ans, pos + 1);
+            swap(nums[i], nums[pos]); // backtrack
+        }
+    }
+
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> ans;
+        prmu(nums, ans, 0);
+        return ans;
+    }
+};
