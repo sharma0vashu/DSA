1849658ac8ea2b93ecc0e30e9080c48a94b5cdc9
Vashu Sharma <149711363+sharma0vashu@users.noreply.github.com>
2025-08-28 20:05:44 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (30.42%) - LeetSync

---
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    void dfs(int i, vector<vector<int>> &adj, vector<int> &vis) {
+        vis[i] = 1;
+        for (auto it : adj[i]) {
+            if (!vis[it]) {
+                dfs(it, adj, vis);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int ans = 0;
+        int n = isConnected.size();
+
+        // adjacency list
+        vector<vector<int>> adj(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (isConnected[i][j] == 1 && i != j) {
+                    adj[i].push_back(j);
+                    adj[j].push_back(i);
+                }
+            }
+        }
+
+        vector<int> vis(n, 0);
+        for (int i = 0; i < n; i++) {
+            if (!vis[i]) {
+                ans++;
+                dfs(i, adj, vis);
+            }
+        }
+
+        return ans;
+    }
+};
